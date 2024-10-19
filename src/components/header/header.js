689a8edc75651fd3ee8507c6a3e06d4bf6e4b430
Leticia Coelho <Leticia.ac23@gmail.com>
2024-10-19 08:51:33 -0300

    Revert "Create a new react router "

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './header.css';
+
+export default class Header extends Component {
+
+  render() {
+    return(
+      <header className="header">
+      </header>
+    )
+  }
+}
